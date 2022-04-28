2f172cadceb95f18c194a31060d2cf4a8dd3f14e
Jack Bluth <bluthjack@gmail.com>
2022-04-27 22:53:49 -0400

    commit tree 716b959d4ed87cb829619763a5f01f6016d8fc8b

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Follow along with the examples here
+function doNothing() {}
+function sayHello() {
+    console.log("Hello!");
+  }
+  sayHello();
+  function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+  }function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+  }
+  function sayHelloToSamip() {
+    console.log("Hello, Samip!");
+  }
+  sayHelloToGuadalupe();
+  sayHelloToLiz();
+  sayHelloToSamip();
+  function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+  }console.log(firstName)
+  function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }
+  say("Goodbye", "Julio");        
+  say("Julio", "hello");
+  function say(greeting, firstName) {
+    console.log("firstName: ", firstName);
+    console.log("greeting: ", greeting);
+    console.log(`${greeting}, ${firstName}!`);
+  }
+  function add(x, y) {
+    return x + y;
+  }
+  console.log(add(1, 2));
+  function add(x, y) {
+    return x + y;
+  }
+  function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+  }
+  console.log(say("Hello", "Liz"));        
